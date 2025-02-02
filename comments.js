fd59e0676ca3fb96bbe39fe75e9702f35dfdcc6d
RebockraAtAtos <95632787+RebockraAtAtos@users.noreply.github.com>
2025-02-02 16:30:05 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,19 +1,19 @@
-// create web server
-const express = require('express');
-const app = express();
+// // create web server
+// const express = require('express');
+// const app = express();
 
-// middleware
-app.use(express.json());
+// // middleware
+// app.use(express.json());
 
-// data
-let comments = [
-  { id: 1, comment: 'hello' },
-  { id: 2, comment: 'world' },
-  { id: 3, comment: 'foo' },
-  { id: 4, comment: 'bar' }
-];
+// // data
+// let comments = [
+//   { id: 1, comment: 'hello' },
+//   { id: 2, comment: 'world' },
+//   { id: 3, comment: 'foo' },
+//   { id: 4, comment: 'bar' }
+// ];
 
-// get all comments
-app.get('/comments', (req, res) => {
-  res.send(comments)
-});
+// // get all comments
+// app.get('/comments', (req, res) => {
+//   res.send(comments)
+// });
